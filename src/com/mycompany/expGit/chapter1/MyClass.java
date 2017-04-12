ecc93304e1b9fb8383e950f438fcc5b4199bfb75
Kathleen Falcon <Kathleen Falcon@132.238.218.218>
2017-04-12 19:29:12 -0400

    Initial Commit

---
+++ src/com/mycompany/expGit/chapter1/MyClass.java
@@ -0,0 +1,31 @@
+/**
+ * 
+ */
+package com.mycompany.expGit.chapter1;
+
+/**
+ * @author Kathleen Falcon
+ *
+ */
+public class MyClass {
+	private String name;
+	private String lastName;
+	
+	public MyClass(String name) {
+		this.name = name;
+	}
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("BEGIN");
+		staticMethod("Kathleen");
+		System.out.println("END");
+	}
+	public static void staticMethod(String inp)
+	{
+		System.out.println("Hi input is " + inp);
+		System.out.println("Bye");
+	}
+}
